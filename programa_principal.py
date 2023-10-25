4928c318fe6bb17fd332d043c403842183b9e9a0
CampoyLira1994 <campoy.lira.1994@gmail.com>
2023-10-24 20:16:52 -0600

    reglas Py

--- programa_principal.py
+++ programa_principal.py
@@ -1,2 +1,15 @@
-# This is a sample Python script.
-print('Hola Mundo')
+# Reglas para definir el nombre de una variable
+# 1. Debe comenzar con letra o _
+# 2. No puede comenzar con un numero
+# 3. Debe contener valores alfanumericos
+# 4. Es sensible a mayusculas/minusculas
+
+mi_variable = 10
+#print(Mi_variable)
+
+print(mi_variable)
+
+_mi_variable = 20
+print(_mi_variable)
+
+#Error, no se permite iniciar el nombre de una variable con un valor numerico 1_mi_variable = 30
